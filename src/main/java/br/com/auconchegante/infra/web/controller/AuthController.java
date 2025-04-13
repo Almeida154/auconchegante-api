ac0cffca4f2cee7cd84bb4b29fd1c1da1164ff1e
David Almeida Santos <davidalmeida154of@gmail.com>
2025-04-13 14:20:47 -0300

    chore: add Swagger configuration

--- src/main/java/br/com/auconchegante/infra/web/controller/AuthController.java
+++ src/main/java/br/com/auconchegante/infra/web/controller/AuthController.java
@@ -3,6 +3,11 @@ package br.com.auconchegante.infra.web.controller;
 import br.com.auconchegante.domain.port.incoming.SignInUseCase;
 import br.com.auconchegante.infra.web.dto.auth.SignInRequest;
 import br.com.auconchegante.infra.web.dto.auth.SignInResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/auth")
+@Tag(name = "Authentication", description = "Authentication endpoints")
 public class AuthController {
     private final SignInUseCase authUseCase;
 
+    @Operation(summary = "Sign in user", description = "Authenticate user with email and password")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = SignInResponse.class)))
     @PostMapping("sign-in")
     ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest request) {
         SignInUseCase.Result result = this.authUseCase
