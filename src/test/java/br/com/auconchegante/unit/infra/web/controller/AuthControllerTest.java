9d91a1f39fa02bf5c2750fede6dbf1855375916c
David Almeida Santos <davidalmeida154of@gmail.com>
2025-04-22 12:06:11 -0300

    test(auth): add unit tests for AuthController

---
+++ src/test/java/br/com/auconchegante/unit/infra/web/controller/AuthControllerTest.java
@@ -0,0 +1,64 @@
+package br.com.auconchegante.unit.infra.web.controller;
+
+import br.com.auconchegante.domain.port.incoming.SignInUseCase;
+import br.com.auconchegante.infra.web.controller.AuthController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTest {
+    @Mock
+    SignInUseCase signInUseCase;
+
+    @InjectMocks
+    AuthController authController;
+
+    private MockMvc mockMvc;
+
+    private static final String TEST_EMAIL = "test@example.com";
+    private static final String TEST_PASSWORD = "password123";
+    private static final String TEST_ACCESS_TOKEN = "test.jwt.token";
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(authController)
+                .build();
+    }
+
+    @Test()
+    @DisplayName("Should call sign in use case and return a token")
+    void signIn() throws Exception {
+        SignInUseCase.Result token = new SignInUseCase.Result(TEST_ACCESS_TOKEN);
+        when(signInUseCase.execute(TEST_EMAIL, TEST_PASSWORD)).thenReturn(token);
+
+        String requestBody = """
+                {
+                    "email": "%s",
+                    "password": "%s"
+                }
+                """.formatted(TEST_EMAIL, TEST_PASSWORD);
+
+        mockMvc.perform(post("/api/auth/sign-in")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value(TEST_ACCESS_TOKEN));
+
+        verify(signInUseCase).execute(TEST_EMAIL, TEST_PASSWORD);
+    }
+}
